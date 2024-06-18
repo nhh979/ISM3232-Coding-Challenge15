96cb8d6499b09b96f8f45e18bb2f1152cb5c2cab
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-06-18 14:18:53 -0400

    Implement dynamic skills section using props.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,21 @@
 import PersonalInfomation from "./PersonalInformation";
-
+import Skills from "./Skills";
+import Experience from "./Experience";
+import Education from "./Education";
 import "./App.css"
+
+const skillList = [
+  {id:1, name: "Python", level: "Advanced"},
+  {id:2, name: "SQL", level: "Advanced"},
+  {id:3, name: "JavaScripts", level: "Intermediate"},
+  {id:4, name: "React", level: "Beginner"},
+]
 
 export default function App() {
   return (
     <div>
       <PersonalInfomation />
+      <Skills skills={skillList}/>
     </div>
   )
 }
