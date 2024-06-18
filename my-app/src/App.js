8c893806277b29ce6d1e61680e544cb3bdb59fad
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-06-18 14:51:59 -0400

    Display educational background in a dedicated section.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -20,6 +20,7 @@ export default function App() {
   return (
     <div>
       <PersonalInfomation />
+      <Education />
       <Skills skills={skillList}/>
       <Experience exps={expList}/>
     </div>
--- my-app/src/Education.js
+++ my-app/src/Education.js
@@ -1,3 +1,9 @@
 export default function Education () {
-    
+    return (
+        <div className='education'>
+            <h3 className='header'>Education</h3>
+            <p className='school'>University of South Florida | Tampa, FL</p>
+            <p className='program'>Mathematics: Computational and Applied</p>
+        </div>
+    )
 }
