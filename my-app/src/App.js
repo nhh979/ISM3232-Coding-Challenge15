3e055767333c7219e80327b743686ad090f515e0
Hieu Nguyen <hieuunguyenn979@gmail.com>
2024-06-18 13:29:26 -0400

    Add personal information section with styled components.

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import PersonalInfomation from "./PersonalInformation";
+
+import "./App.css"
+
+export default function App() {
+  return (
+    <div>
+      <PersonalInfomation />
+    </div>
+  )
+}
---
+++ my-app/src/PersonalInformation.js
@@ -0,0 +1,16 @@
+
+export default function PersonalInfomation() {
+    return (
+        <div className='personalInfo'>
+        <h1 className='name'>Hoang Nguyen</h1>
+        <h2 className='title'>A Data Analyst</h2>
+        <h3>About me</h3>
+        <p className='biography'>Hoang is a junior at USF, majoring in Mathematics: Computational and Applied. He has a strong background
+            in statistics and is proficient in programming languages such as Python and SQL. <br/>As a self-taught data analyst 
+            he is passionate about pursing a career in the data science field. </p>
+        <h3>Contact</h3>
+        <p className='contactInfo'><strong>Email:</strong> hnguyen171@usf.edu</p>
+        <p className='contactInfo'><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/hieunguyen2310/" target='blank'>https://www.linkedin.com/in/hieunguyen2310/</a></p>
+    </div>
+    )
+}
